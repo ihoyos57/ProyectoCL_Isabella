b959b1037581832127e148b3b19a49e1e62a26c7
Isa <ihoyos573@soyudemedellin.edu.co>
2023-04-15 14:38:34 -0500

    Se crea clase personal con las posibles propiedades que defininen el objeto persona

---
+++ HistoriaClinica/Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Web;
+//using System.Web.UI.WebControls;
+
+namespace HistoriaClinica.Models
+{
+    public class Persona
+    {
+        public Persona(string nombre, string apellidos, int id, DateTime fechaNacimiento) 
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.id = id;
+            this.fechaNacimiento = fechaNacimiento;
+            this.edad = DateTime.Today.Year - fechaNacimiento.Year;
+        }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public int id { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public int edad { get; set; }
+        private string genero { get; set; }
+    }
+}
